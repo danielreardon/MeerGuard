0481c1e00221bbd7fa49885ecd2b6b17501f696e
Patrick Lazarus <plazarus@mpifr.de>
2012-02-26 22:47:21 +0100

    Add new utilities.
    Added utils for:
    1) Getting info from code's git repo.
    2) Converting MJDs to dates
    3) Interpolating file names.

--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ parts of the CoastGuard timing pipeline.
 
 Patrick Lazarus, Nov. 10, 2011
 """
+import os
+import os.path
+import warnings
+import hashlib
 import glob
 import optparse
 import sys
@@ -35,6 +39,64 @@ site_to_telescope = {'i': 'WSRT',
                      'nancay': 'Nancay',
                      'sardinia': 'SRT',
                      'srt': 'SRT'}
+
+def get_md5sum(fn, block_size=16*8192):
+    """Compute and return the MD5 sum for the given file.
+        The file is read in blocks of 'block_size' bytes.
+
+        Inputs:
+            fn: The name of the file to get the md5 for.
+            block_size: The number of bytes to read at a time.
+                (Default: 16*8192)
+
+        Output:
+            md5: The hexidecimal string of the MD5 checksum.
+    """
+    f = open(fn, 'rb')
+    md5 = hashlib.md5()
+    block = f.read(block_size)
+    while block:
+        md5.update(block)
+        block = f.read(block_size)
+    f.close()
+    return md5.hexdigest()
+
+
+def get_githash():
+    """Get the Coast Guard project's git hash.
+
+        Inputs:
+            None
+
+        Output:
+            githash: The githash
+    """
+    if is_gitrepo_dirty():
+        warnings.warn("Git repository has uncommitted changes!")
+    codedir = os.path.split(__file__)[0]
+    stdout, stderr = execute("git rev-parse HEAD", dir=codedir)
+    githash = stdout.strip()
+    return githash
+
+
+def is_gitrepo_dirty():
+    """Return True if the git repository has local changes.
+
+        Inputs:
+            None
+
+        Output:
+            is_dirty: True if git repository has local changes. False otherwise.
+    """
+    codedir = os.path.split(__file__)[0]
+    try:
+        stdout, stderr = execute("git diff --quiet", dir=codedir)
+    except errors.SystemCallError:
+        # Exit code is non-zero
+        return True
+    else:
+        # Success error code (i.e. no differences)
+        return False
 
 
 def get_header_vals(fn, hdritems):
@@ -196,6 +258,84 @@ def get_files_from_glob(option, opt_str, value, parser):
     glob_file_list.extend(glob.glob(value))
 
 
+def get_outfn(fmtstr, arfn):
+    """Replace any format string codes using file header info
+        to get the output file name.
+
+        Inputs:
+            fmtstr: The string to replace header info into.
+            arfn: An archive file name to get header info from using vap.
+
+        Output:
+            outfn: The output filename with (hopefully) all
+                format string codes replace.
+    """
+    if '%' not in fmtstr:
+        # No format string codes
+        return fmtstr
+
+    # Get header information
+    hdr = get_header_vals(arfn, ['freq', 'telescop', 'site', \
+                                 'rcvr', 'backend', 'name', \
+                                 'mjd', 'intmjd', 'fracmjd'])
+    # Cast some values and compute others
+    hdr['mjd'] = float(hdr['mjd'])
+    hdr['freq'] = float(hdr['freq'])
+    hdr['intmjd'] = float(hdr['intmjd'])
+    hdr['fracmjd'] = float(hdr['fracmjd'])
+    hdr['secs'] = int(hdr['fracmjd']*24*3600)
+    hdr['yyyymmdd'] = "%04d%02d%02d" % mjd_to_date(hdr['mjd'])
+    outfn = fmtstr % hdr
+
+    if '%' in outfn:
+        raise errors.BadFileName("Interpolated file name (%s) shouldn't " \
+                                 "contain the character '%%'!" % outfn)
+    return outfn
+
+def mjd_to_date(mjds):
+    """Convert Julian Day (JD) to a date.
+
+        Input:
+            mjds: Array of Modified Julian days
+
+        Outputs:
+            years: Array of years.
+            months: Array of months.
+            days: Array of (fractional) days.
+
+        (Follow Jean Meeus' Astronomical Algorithms, 2nd Ed., Ch. 7)
+    """
+    JD = np.atleast_1d(mjds)+2400000.5
+
+    if np.any(JD<0.0):
+        raise ValueError("This function does not apply for JD < 0.")
+
+    JD += 0.5
+
+    # Z is integer part of JD
+    Z = np.floor(JD)
+    # F is fractional part of JD
+    F = np.mod(JD, 1)
+
+    A = np.copy(Z)
+    alpha = np.floor((Z-1867216.25)/36524.25)
+    A[Z>=2299161] = Z + 1 + alpha - np.floor(0.25*alpha)
+
+    B = A + 1524
+    C = np.floor((B-122.1)/365.25)
+    D = np.floor(365.25*C)
+    E = np.floor((B-D)/30.6001)
+
+    day = B - D - np.floor(30.6001*E) + F
+    month = E - 1
+    month[(E==14.0) | (E==15.0)] = E - 13
+    year = C - 4716
+    year[(month==1.0) | (month==2.0)] = C - 4715
+
+    return (year.astype('int').squeeze(), month.astype('int').squeeze(), \
+                day.squeeze())
+
+
 class DefaultOptions(optparse.OptionParser):
     def __init__(self, *args, **kwargs):
         optparse.OptionParser.__init__(self, *args, **kwargs)
@@ -242,7 +382,6 @@ class DefaultOptions(optparse.OptionParser):
 
     def toggle_colours(self, option, opt_str, value, parser):
         config.colour = not config.colour
-
 
     def debug_callback(self, option, opt_str, value, parser, mode):
         config.debug.set_mode_on(mode)
