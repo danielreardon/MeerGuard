575e606e00236da08bcb592ede9f7f04dbd8018d
Patrick Lazarus <plazarus@mpifr.de>
2012-03-05 15:38:46 +0100

    'correct_header.sh' now writes out pam calls.

--- correct_header.sh
+++ correct_header.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+pamcalls=$1
+
+echo "# $(date)" >> $pamcalls
+
 for png in $(ls *.ar.png); do
     fn=$(basename ${png} .png)
     display ${png} &
@@ -6,12 +10,15 @@ for png in $(ls *.ar.png); do
     case $RCVR in
     "l")
         echo ${fn} "LBAND"
+        echo pam -m --receiver lband.rcvr --inst Asterix ${fn} >> $pamcalls
         ;;
     "s")
         echo ${fn} "SBAND"
+        echo pam -m --receiver sband.rcvr --inst Asterix ${fn} >> $pamcalls
         ;;
     "7")
         echo ${fn} "7BEAM"
+        echo pam -m --receiver 7beam.rcvr --inst Asterix ${fn} >> $pamcalls
         ;;
     *)
         echo "Unrecognized selection ${RCVR}. You suck" 1>&2
