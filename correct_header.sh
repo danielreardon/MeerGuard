b8f78703b9757965cfd1bc8648a9249a28eb807f
Patrick Lazarus <plazarus@mpifr.de>
2012-03-14 12:12:43 +0100

    Added features to 'correct_header.sh' script.

--- correct_header.sh
+++ correct_header.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
+
 pamcalls=$1
-
 echo "# $(date)" >> $pamcalls
+shift
 
-for png in $(ls *.ar.png); do
+while (( "$#" )); do
+    # Set up filenames
+    png=$1
     fn=$(basename ${png} .png)
+    
+    # Display the plot
+    echo "Displaying ${png}"
     display ${png} &
+    
+    # Get receiver code and interpret it
     read -N 1 -s -p "Type receiver code> " RCVR
     case $RCVR in
     "l")
         echo ${fn} "LBAND"
-        echo pam -m --receiver lband.rcvr --inst Asterix ${fn} >> $pamcalls
+        echo pam -e rcvr --receiver lband_rcvr.txt --inst Asterix ${fn} >> $pamcalls
         ;;
     "s")
         echo ${fn} "SBAND"
-        echo pam -m --receiver sband.rcvr --inst Asterix ${fn} >> $pamcalls
+        echo pam -e rcvr --receiver sband_rcvr.txt --inst Asterix ${fn} >> $pamcalls
         ;;
     "7")
         echo ${fn} "7BEAM"
-        echo pam -m --receiver 7beam.rcvr --inst Asterix ${fn} >> $pamcalls
+        echo pam -e rcvr --receiver 7beam_rcvr.txt --inst Asterix ${fn} >> $pamcalls
         ;;
     *)
         echo "Unrecognized selection ${RCVR}. You suck" 1>&2
+        echo "# Unrecognized selection (${RCVR}) for ${fn}"
     esac
+    
+    # Clean up and prepare for next interation of loop
     kill $!
+    shift
 done
