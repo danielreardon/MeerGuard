f5920efd13b59a360c85460e1d0f89e65e6219df
Patrick Lazarus <plazarus@mpifr.de>
2013-01-24 11:33:16 +0100

    Add a script to generate composite plots.

---
+++ composite_plot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import utils
+import diagnose
+
+def main():
+    for arfn in args:
+        print "Plotting %s" % arfn,
+        diagnose.make_composite_summary_plot(arfn, \
+                    options.preproc, options.outpsfn)
+        print " Done"
+
+
+if __name__ == '__main__':
+    parser = utils.DefaultOptions()
+    parser.add_option('-j', dest='preproc', \
+        help="'psrplot' recognized preprocessing commands. (Default: 'C,D')", \
+        default='C,D')
+    parser.add_option('-o', dest='outpsfn', \
+        help="Output postscript file name. (Default: <archive name>.ps", \
+        default=None)
+    options, args = parser.parse_args()
+    main()
+
