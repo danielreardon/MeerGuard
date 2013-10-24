1733689dbc1bebadd4b2070091208c90df5f2343
Patrick Lazarus <plazarus@mpifr.de>
2013-10-24 11:32:08 +0200

    Add new statuses and two new tables (one for logs and another for diagnostics).

--- database/schema.py
+++ database/schema.py
@@ -1,8 +1,8 @@
 import sqlalchemy as sa
 
 DIRECTORY_STATUSES = ['new', 'failed', 'grouped', 'archived']
-GROUPING_STATUSES = ['new', 'failed', 'combined']
-FILE_STATUSES = ['new', 'failed', 'processed']
+GROUPING_STATUSES = ['new', 'failed', 'combined', 'running']
+FILE_STATUSES = ['new', 'failed', 'running', 'processed', 'done', 'diagnosed']
 FILE_OBSTYPES = ['pulsar', 'cal']
 FILE_STAGES = ['combined', 'corrected', 'cleaned', 'calibrated']
 
@@ -27,7 +27,6 @@ sa.Table('directories', metadata, \
                     autoincrement=True, nullable=False), \
         sa.Column('path', sa.String(512), nullable=False, \
                     unique=True), \
-#        sa.Column('status', sa.String(32), nullable=False, \
         sa.Column('status', sa.Enum(*DIRECTORY_STATUSES), nullable=False, \
                     default='new'), \
         sa.Column('added', sa.DateTime, nullable=False, \
@@ -51,6 +50,36 @@ sa.Table('groupings', metadata, \
                     default='new'), \
         sa.Column('md5sum', sa.String(64), nullable=False, \
                     unique=True), \
+        sa.Column('added', sa.DateTime, nullable=False, \
+                    default=sa.func.now()), \
+        sa.Column('last_modified', sa.DateTime, nullable=False, \
+                    default=sa.func.now()), \
+        mysql_engine='InnoDB', mysql_charset='ascii')
+
+
+# Define diagnostics table
+sa.Table('diagnostics', metadata, \
+        sa.Column('diagnostic_id', sa.Integer, primary_key=True, \
+                    autoincrement=True, nullable=False), \
+        sa.Column('file_id', sa.Integer, \
+                    sa.ForeignKey("files.file_id", name="fk_diag_file")), \
+        sa.Column('diagnosticpath', sa.String(512), nullable=False), \
+        sa.Column('diagnosticname', sa.String(512), nullable=False), \
+        sa.Column('added', sa.DateTime, nullable=False, \
+                    default=sa.func.now()), \
+        sa.Column('last_modified', sa.DateTime, nullable=False, \
+                    default=sa.func.now()), \
+        mysql_engine='InnoDB', mysql_charset='ascii')
+
+
+# Define logs table
+sa.Table('logs', metadata, \
+        sa.Column('log_id', sa.Integer, primary_key=True, \
+                    autoincrement=True, nullable=False), \
+        sa.Column('group_id', sa.Integer, \
+                    sa.ForeignKey("groupings.group_id", name="fk_log_group")), \
+        sa.Column('logpath', sa.String(512), nullable=False), \
+        sa.Column('logname', sa.String(512), nullable=False), \
         sa.Column('added', sa.DateTime, nullable=False, \
                     default=sa.func.now()), \
         sa.Column('last_modified', sa.DateTime, nullable=False, \
