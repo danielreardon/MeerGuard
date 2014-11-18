e836bc38b19b2186b79c2232b3b34bfe0eeb47cf
Patrick Lazarus <plazarus@mpifr.de>
2014-11-19 00:08:44 +0100

    Update 'check_detections.py' with Eleni's changes.

--- check_detections.py
+++ check_detections.py
@@ -2,9 +2,12 @@
 
 import matplotlib.pyplot as plt
 
+from coast_guard import config
 from coast_guard import database
 from coast_guard import utils
 
+import numpy as np
+from math import isinf
 
 def get_files(psrname):
     """Get a list of database rows the given pulsar.
@@ -17,14 +20,13 @@ def get_files(psrname):
                 information for each matching file.
     """
     db = database.Database()
+    print "Before", psrname
     psrname = utils.get_prefname(psrname)
+    print "After", psrname
+   
 
     whereclause = (db.obs.c.sourcename == psrname) & \
                   (db.files.c.stage == 'cleaned') & \
-                  (((db.files.c.status == 'failed') &
-                    (db.files.c.qcpassed == False)) |
-                   ((db.files.c.status.in_(['new', 'processed', 'done'])) &
-                    (db.files.c.qcpassed == True))) & \
                   (db.obs.c.obstype == 'pulsar')
     with db.transaction() as conn:
         select = db.select([db.files,
@@ -43,30 +45,99 @@ def get_files(psrname):
         result.close()
     return rows
 
+       
 
 def main():
-    rows = get_files(args.psrname)
-    data = {'detect': ([], []),
-            'RFI': ([], []),
-            'non-detect': ([], [])}
-    for row in rows:
-        if row['qcpassed']:
-            if row['snr']:
-                data['detect'][0].append(row['start_mjd'])
-                data['detect'][1].append(row['snr'])
-        else:
-            if 'RFI' in row['note']:
-                data['RFI'][0].append(row['start_mjd'])
-                data['RFI'][1].append(0)
-            elif 'non-detection' in row['note']:
-                data['non-detect'][0].append(row['start_mjd'])
-                data['non-detect'][1].append(0)
-    plt.figure()
-    plt.scatter(data['detect'][0], data['detect'][1], marker='o', c='k')
-    print data['RFI'][0], data['RFI'][1]
-    plt.scatter(data['RFI'][0], data['RFI'][1], marker='x', c='r')
-    plt.scatter(data['non-detect'][0], data['non-detect'][1], marker='o', facecolors=None)
-    plt.show()
+    #psrnames = open(args.psrfile)   
+
+    psrname = args.psrname
+    if True:
+	rows = get_files(psrname)  
+     
+        data = {'detect-L': ([], []),
+                'RFI-L': ([], []),
+                'non-detect-L': ([], []),
+    	        'detect-S': ([], []),
+                'RFI-S': ([], []),
+                'non-detect-S': ([], [])}
+        for row in rows:
+	    if row['rcvr'] == 'S110-1':
+	        rx = "-S"
+            else:
+                rx = "-L"
+            if row['qcpassed']:
+                if row['snr']:
+                    data['detect'+rx][0].append(row['start_mjd'])
+                    data['detect'+rx][1].append(row['snr'])
+            else:
+                if (row['note'] is not None) and ('RFI' in row['note']):
+                    data['RFI'+rx][0].append(row['start_mjd'])
+                    data['RFI'+rx][1].append(0)
+                else:
+                    data['non-detect'+rx][0].append(row['start_mjd'])
+                    data['non-detect'+rx][1].append(0)
+                
+        plt.figure(1)
+        for ii, rx in enumerate(['-L', '-S'], 1):
+	    plt.subplot(2,1,ii)
+	    plt.title("%s-band" % (rx[1]))
+            plt.scatter(data['detect'+rx][0], data['detect'+rx][1], marker='o', c='k')
+            plt.scatter(data['RFI'+rx][0], data['RFI'+rx][1], marker='x', c='r')
+            plt.scatter(data['non-detect'+rx][0], data['non-detect'+rx][1], marker='o', facecolors='none')
+	    plt.xlabel("MJD")
+ 	    plt.ylabel("snr")
+	    if data['detect'+rx][1]:
+	    	print "Detections", data['detect'+rx][1], "\n", max(data['detect'+rx][1])
+		plt.ylim(0 - 0.1*max(data['detect'+rx][1]),  max(data['detect'+rx][1]) + 0.1*max(data['detect'+rx][1]) )
+		plt.xlim(55564, max(data['detect'+rx][0]) + 100)
+	    else:
+		print "Detections", data['detect'+rx][1], "\n"
+		plt.ylim(-4,100)
+		plt.xlim(55564, 57023)
+        plt.suptitle(psrname, fontsize=14)
+#     	plt.show(1)
+	plt.figure(2)
+	for ii, rx in enumerate(['-L', '-S'], 1):
+	    if data['detect'+rx][1] + data['RFI'+rx][1] + data['non-detect'+rx][1]:
+            	plt.subplot(2,2,ii)
+            	plt.title("%s-band" % (rx[1]))
+            	plt.hist(data['detect'+rx][1] + data['RFI'+rx][1] + data['non-detect'+rx][1],15)
+	    	plt.xlabel("snr")
+	    	print "combineddata", data['detect'+rx][1] + data['RFI'+rx][1] + data['non-detect'+rx][1]
+#	    	logdata = np.log10(data['detect'+rx][1] + data['RFI'+rx][1] + data['non-detect'+rx][1])
+#	    	print logdata	    
+#	    	for i in range(len(logdata)):
+#			if isinf(logdata[i]):
+#				print logdata[i]
+#				logdata[i] = 0.1
+#				print "After", logdata[i]		
+
+#	    	print np.log10(logdata)
+	    	plt.subplot(2,2,ii+2)
+	    	print "plot number", ii+2
+		hist, bins = np.histogram(data['detect'+rx][1] + data['RFI'+rx][1] + data['non-detect'+rx][1],15)
+		print "hist", hist
+	    	percentage = (100.0 * np.array(hist[:]))/np.array(sum(hist[:]))
+	    	widths = np.diff(bins)
+	    	print "snr bins", bins[:-1], "\n", "percentage", percentage, "\n", widths
+#	    	snrbins = pow(10,bins)
+	    	plt.bar(bins[:-1], percentage, widths) 
+	    	plt.xlabel("snr") 
+	    	plt.ylabel("percentage (%)")
+
+	    else:
+		plt.title("%s-band" % (rx[1]))
+		plt.subplot(2,2,ii)
+		plt.scatter(data['detect'+rx][0], data['detect'+rx][1], marker='o', c='k')
+   		plt.ylim(0,30)
+                plt.xlim(0,20)
+		plt.subplot(2,2,ii+2)
+		plt.scatter(data['detect'+rx][0], data['detect'+rx][1], marker='o', c='k')
+                plt.ylim(0,30)
+                plt.xlim(0,20)
+
+	plt.suptitle(psrname, fontsize=14)
+   	plt.show()
 
 
 if __name__ == '__main__':
@@ -74,5 +145,10 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--psr', dest='psrname', type=str,
                         required=True,
                         help='Name of the pulsar to check.')
+#    parser.add_argument('--file', dest='psrlist', type=str,
+#                        required=True,
+#                        help='sfsfdf')
+    parser.add_argument('--files', dest='psrfile', type=str, 
+			 help="gfger")   
     args = parser.parse_args()
-    main()
+    main()             
