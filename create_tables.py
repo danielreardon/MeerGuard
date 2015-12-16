2d74bf47ae95deba78614da5557f9a815c69d36d
Patrick Lazarus <plazarus@mpifr.de>
2015-12-16 18:45:27 +0100

    Update create_tables.py to specifically create coast_guard database.

--- create_tables.py
+++ create_tables.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import database
 import utils
+import config
+
 
 def main():
-    engine = database.get_engine()
+    engine = database.get_engine(config.dburl)
     database.schema.metadata.create_all(engine)
+            # Add this argument to "create_all" to make specific tables:
+            # tables=[database.schema.metadata.tables['qctrl'], database.schema.metadata.tables['reattempts']]
 
 
 if __name__=='__main__':
