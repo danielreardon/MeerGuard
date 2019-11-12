5806a3b5f3ed79427d7947a3a927ce43c09b0293
Bradley Meyers <bradley.meyers1993@gmail.com>
2019-11-12 15:10:55 -0500

    made surgical cleaner a bit more verbose and informative

--- coast_guard/cleaners/surgical.py
+++ coast_guard/cleaners/surgical.py
@@ -87,6 +87,7 @@ class SurgicalScrubCleaner(cleaners.BaseCleaner):
         # Remove profile from dedispersed data
         patient.dedisperse()
         data = patient.get_data().squeeze()
+        print('Loading template')
         if self.configs.template is None:
             template = np.apply_over_axes(np.sum, data, (0, 1)).squeeze()
         else:
@@ -100,12 +101,13 @@ class SurgicalScrubCleaner(cleaners.BaseCleaner):
             template = np.apply_over_axes(np.sum, template_ar.get_data(), (0, 1)).squeeze()
             # make sure template is 1D
             if len(np.shape(template)) > 1:  # sum over frequencies too
-                template_ar.fscrunch()
-                print("2D template found. Assuming it has same frequency coverage and channels  as data!")
+                template_ar.fscrunch()  
+                print("2D template found. Assuming it has same frequency coverage and channels as data!")
                 template_phs = np.apply_over_axes(np.sum, template_ar.get_data(), (0, 1)).squeeze()
             else:
                 template_phs = template
 
+        print('Estimating template and profile phase offset')
         if self.configs.template is None:
             phs = 0
         else:
@@ -123,15 +125,48 @@ class SurgicalScrubCleaner(cleaners.BaseCleaner):
             phase_guess = - np.argmax(profile) + np.argmax(template_phs)
             params, status = leastsq(err, [amp_guess, phase_guess, min(profile)])
             phs = params[1]
-            print params[2]
+            #print params[2]
             print('Template phase offset = {0}'.format(round(phs, 3)))
 
+        print('Removing profile from patient')
+        preop_patient = patient.clone()
+        preop_patient.remove_baseline()
+        preop_weights = preop_patient.get_weights()
+        #print(preop_weights.shape)
+
         clean_utils.remove_profile_inplace(patient, template, phs)
-        # re-set DM to 0
+       
+#        import sys
+#        import matplotlib.pyplot as plt
+#        
+#        fig, (ax1, ax2) = plt.subplots(ncols=2, sharex="all", sharey="all")
+#        # before profile removal
+#        combined = np.array([np.mean(preop_patient.get_data()[:,0,:,:], axis=0), np.mean(patient.get_data()[:,0,:,:], axis=0)])
+#        _min, _max = np.amin(combined), np.amax(combined)
+#        ax1.imshow(np.mean(preop_patient.get_data()[:,0,:,:], axis=0), origin="lower", aspect="auto", vmin=_min, vmax=_max)
+#        ax1.set_xlabel("Phase bin")
+#        ax1.set_ylabel("Frequency channel")
+#        ax1.set_title("Pre-Op patient")
+#        ax2.imshow(np.mean(patient.get_data()[:,0,:,:], axis=0), origin="lower", aspect="auto", vmin=_min, vmax=_max)
+#        ax2.set_xlabel("Phase bin")
+#        ax2.set_title("Post-Op patient")
+#
+#        #plt.figure()
+#        #plt.plot(preop_weights.sum(axis=0))
+#        #plt.plot(patient.get_weights().sum(axis=0))
+#        plt.savefig("preop_postop_removal.png")
+#
+#        #fig, (ax1, ax2) = plt.subplots(ncols=2)
+#        #ax1.plot(preop_patient.get_Profile().get_amps())
+#        #ax2.plot(patient.get_amps())
+#        #plt.show()
+# 
+#        #sys.exit(0)
+       # re-set DM to 0
         patient.dededisperse()
 
-
-
+       
+        print('Accessing weights and applying to patient')
         # Get weights
         weights = patient.get_weights()
         # Get data (select first polarization - recall we already P-scrunched)
@@ -143,7 +178,9 @@ class SurgicalScrubCleaner(cleaners.BaseCleaner):
         mask_3d = mask_2d.repeat(ar.get_nbin(), axis=2)
         data = np.ma.masked_array(data, mask=mask_3d)
 
+        print('Calculating robust statistics to determine where RFI removal is required')
         # RFI-ectomy must be recommended by average of tests
+        # BWM: Ok, so this is where the magical stuff actually happens - need to know actually WHAT are the comprehensive stats
         avg_test_results = clean_utils.comprehensive_stats(data, axis=2, \
                                     chanthresh=self.configs.chanthresh, \
                                     subintthresh=self.configs.subintthresh, \
@@ -154,13 +191,21 @@ class SurgicalScrubCleaner(cleaners.BaseCleaner):
                                     subint_breakpoints=self.configs.subint_breakpoints, \
                                     subint_numpieces=self.configs.subint_numpieces, \
                                     )
+
+#        print(avg_test_results.shape)
+#        plt.plot(avg_test_results.sum(axis=0))
+#        plt.show()
+
+        print('Applying RFI masking weights to archive')
         for (isub, ichan) in np.argwhere(avg_test_results>=1):
+            #print('(isub, ichan) to be flagged: ({0}, {1})'.format(isub, ichan))
+            #print('     test score = {0}'.format(avg_test_results[isub, ichan]))
             # Be sure to set weights on the original archive, and
             # not the clone we've been working with.
             integ = ar.get_Integration(int(isub))
             integ.set_weight(int(ichan), 0.0)
         
-        freq_fraczap=  clean_utils.freq_fraczap(ar)
+        freq_fraczap = clean_utils.freq_fraczap(ar)
     
         #print np.shape(freq_fraczap)
 
